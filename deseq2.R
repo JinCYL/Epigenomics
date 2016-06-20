cd147991181975b5d994275635e2987d761fa3fe
Guanjing Hu <huguanjing@gmail.com>
2016-06-20 15:44:45 -0500

    Update deseq2.R

--- deseq2.R
+++ deseq2.R
@@ -2,37 +2,82 @@
 ## Differential Expression Analysis ##
 ######################################
 
-getwd()  # "/home/hugj2006/ML/htseq_count"
-files<-list.files()
+# generate read ount table from htseq-count results
+setwd("/home/hugj2006/ML/htseq_count")
+files<-grep(".namesort.bam.txt",list.files(),value=TRUE)
 for(f in files)
 {
     x <- read.table(f,sep="\t")
-    names(x) <- c("gene", f)
+    names(x) <- c("gene", gsub(".namesort.bam.txt","",f))
     if(!exists("allcount")) {allcount = x}
     else {allcount <- merge(allcount, x, by="gene")}
 }
+write.table(allcount, file="count.raw.txt", row.names=FALSE, sep="\t")
+
+
+allcount<-read.table(file="count.raw.txt", header=TRUE, sep="\t")
 row.names(allcount) <- allcount$gene
-write.table(all.count, file="count.raw.txt", row.names=FALSE, sep="\t")
-
-
-count<-read.table(file="count.raw.txt", header=TRUE, sep="\t")
 count13<-allcount[grep("Gorai.0",allcount$gene,value=TRUE),-1]
 dim(count13) # 37223    47
 
+# prepare column information table
 coldata<- as.data.frame(t(allcount[grep("__",allcount$gene,value=TRUE),-1]))
-coldata$sample<-gsub(".namesort.bam.txt","",rownames(coldata))
+coldata$sample<-rownames(coldata)
 coldata$flowering <-"F"
 coldata$flowering[grep("NF",coldata$sample)] <-"NF"
-coldata$condition<-gsub("-.*","",rownames(coldata))
-coldata$sample<-gsub("-S.*|-L.*","",coldata$sample)
-coldata$genome<-gsub(".*-","",coldata$sample)
+coldata$condition<-gsub("[.].*","",rownames(coldata))
+coldata$sample<-gsub("[.]S.*|[.]L.*","",coldata$sample)
+coldata$genome<-gsub(".*[.]","",coldata$sample)
 
-
+# prepare for exploratory plots
+library(genefilter)
+sumPCA<-
+function (x, genes, intgroup = "condition", ntop = 500)
+{
+    rv = rowVars(assay(x)[genes,])
+    select = order(rv, decreasing = TRUE)[seq_len(min(ntop, length(rv)))]
+    pca = prcomp(t(assay(x)[genes,][select, ]))
+    tt = pca$x[,c("PC1","PC2")]
+    tt = cbind(tt, coldata[rownames(tt),] )
+    attr(tt,"percentVar")<-summary(pca)$importance[2,1:2]
+    return(tt)
+}
+# consider flowering time genes from Corrinne
+ftgenes<-as.character(read.table("~/ML/floweringtime.genes")$V1 )
 library(DESeq2)
+library(ggplot2)
 dds <- DESeqDataSetFromMatrix( countData = count13, colData = coldata, design = ~ sample)
 rld <- rlog(dds, blind=FALSE)
-plotPCA(rld, intgroup=c("condition", "genome"))
 
+# now ploting
+pdf("MLplots.pdf")
+### 1: plotPCA with top 500, default
+plotPCA(rld, intgroup=c("condition", "genome"))
+data <- plotPCA(rld, intgroup=c("condition", "genome"), returnData=TRUE)
+percentVar <- round(100 * attr(data, "percentVar"))
+### 2. same as plotPCA with shape
+ggplot(data, aes(PC1, PC2, color=condition, shape=genome)) +
+geom_point(size=3) +
+xlab(paste0("PC1: ",percentVar[1],"% variance")) +
+ylab(paste0("PC2: ",percentVar[2],"% variance")) +
+ggtitle("Top 500 genes - plotPCA default")
+### 3. consider only flowing genes
+data <- sumPCA(rld, genes= ftgenes, intgroup=c("condition", "genome"), ntop = length(ftgenes) )
+percentVar <- round(100 * attr(data, "percentVar"))
+ggplot(data=data, aes(PC1, PC2, color=condition, shape=genome)) +
+geom_point(size=3) +
+xlab(paste0("PC1: ",percentVar[1],"% variance")) +
+ylab(paste0("PC2: ",percentVar[2],"% variance")) +
+ggtitle("Flowering genes")
+### 4. Heatmap of the count matrix for flower genes
+library("pheatmap")
+df <- as.data.frame(colData(dds)[,c("condition","type")])
+pheatmap(assay(rld)[ftgenes,], cluster_rows=TRUE, show_rownames=FALSE,cluster_cols=TRUE, annotation_col=coldata[,c("condition","genome")], fontsize_row = 4, fontsize_col = 8)
+###
+dev.off()
+###########   END
+
+# Expression profiles of flowering genes are dominated by difference between sunrise(LD7 and SD7) and sunset(LD9 and SD5)
 
 
 # pairwise deseq workflow
